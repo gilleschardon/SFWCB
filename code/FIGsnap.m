886a9d615cc2bc240f5e5795022c85b0bf15777e
gilles <gilles.chardon@m4x.org>
2021-09-26 11:49:52 +0200

    figsnap

--- code/FIGsnap.m
+++ code/FIGsnap.m
@@ -20,13 +20,14 @@ sample_size = 50;
 nbPlots = length(snapshots);
 
 % position errors (NOMP SFW OMP)
-Errors_p=zeros(3,nbPlots, sample_size);
-% amplitude errors
-Errors_q=zeros(3,nbPlots, sample_size);
+Errors_p=zeros(4,nbPlots, sample_size);
+% amplitude err5ors
+Errors_q=zeros(4,nbPlots, sample_size);
 
 % running time
 TN = zeros(nbPlots,1); % NOMP
 TS = zeros(nbPlots, 1); % SFW
+
 TO = zeros(nbPlots, 1); % OMP
 
 tol = 1e-9; % NOMP stopping criterion
@@ -46,6 +47,16 @@ zz = (LBz:step:UBz)';
 [Xg, Yg, Zg] = meshgrid(xx, yy, zz);
 
 XX = [Xg(:) Yg(:) Zg(:)];
+
+
+stepM = 0.05;
+xxM = (LBx:stepM:UBx)';
+yyM = (LBy:stepM:UBy)';
+zzM = (LBz:stepM:UBz)';
+[Xgm, Ygm] = meshgrid(xxM, yyM);
+[Xgm3, Ygm3, Zgm3] = meshgrid(xxM, yyM, zzM);
+
+XXM = [Xgm3(:) Ygm3(:) Zgm3(:)];
 
 % for the waitbar
 ntotal = nbPlots * sample_size;
@@ -87,16 +98,40 @@ for p = 1:nbPlots
         tic
         [XSFW, RE, IM] = sfw_multi_norm(Pmic, k, Y, XX, 0, 0, 0, nbSources, [LBx LBy LBz]-0.1, [UBx UBy UBz]+0.1);
         TS(p) = TS(p) + toc;
-        [epS, eaS ] = compute_errors(XSFW, XS, sqrt(RE.^2+IM.^2), a);
+        
+        Dl = dictionary(Pmic, XSFW, k);
+        norms = sqrt(sum(abs(Dl).^2, 1));
 
+        Asfw = sqrt(RE.^2+IM.^2) ./ norms';
+
+        
+        [epS, eaS ] = compute_errors(XSFW, XS, Asfw, a);
+
+        
+        
+      
+        
         % OMP
         tic
         [xomp, q_OMP] = OMP(Y,nbSources,XX, Pmic, k);
         [epO, eaO] = compute_errors(xomp, XS, q_OMP, a);
         TO(p) = TO(p) + toc;
+        
+        
 
-        Errors_p(:,p, s)=[epN, epS, epO]';
-        Errors_q(:,p, s)=[eaN, eaS, eaO]';      
+epM = inf;
+eaM = inf;
+    if snapshots(p) > 3
+        Xmusic = MUSIC_local(Y, nbSources,XXM,Pmic, k);
+        Dmusic = dictionary(Pmic, Xmusic, k);
+        [epM, eaM] = compute_errors_music(Xmusic, XS, Y, a, Dmusic);
+    end
+
+
+    
+          Errors_p(:,p, s) = [epN, epS, epO, epSl, epM];
+        Errors_q(:,p, s)= [eaN, eaS, eaO, eaSl, eaSlun, eaM];
+        
     end 
 end
 
@@ -104,34 +139,37 @@ end
 Errors_p_m= mean(Errors_p, 3);
 Errors_q_m= mean(Errors_q, 3);
 
-save snap
+%save snap
 %% Plot the points
 load snap
 
 figure('Position', [100, 100, 800, 300])
-
+msize = 15;
 subplot(1,2,1);
 hold on
 set(gca, 'YScale', 'log')
-plot(snapshots,Errors_p_m(2,:),'-o','LineWidth',2, 'markersize', 10);
+plot(snapshots,Errors_p_m(2,:),'-o','LineWidth',2, 'markersize', msize);
 set(gca, 'ColorOrderIndex', get(gca, 'ColorOrderIndex')+1);
-plot(snapshots,Errors_p_m(1,:),'--x','LineWidth',2, 'markersize', 10);
-plot(snapshots,Errors_p_m(3,:),':s','LineWidth',2, 'markersize', 10);
+plot(snapshots,Errors_p_m(1,:),'--x','LineWidth',2, 'markersize', msize);
+plot(snapshots,Errors_p_m(3,:),':s','LineWidth',2, 'markersize', msize);
+
+
+plot(snapshots,Errors_p_m(4,:),':*','LineWidth',2, 'markersize', msize);
 
 xlabel("Snapshots")
 ylabel("MSE position (m^2) ")
-legend('SFWm','NOMP', 'OMP')
 yticks([0.0001, 0.001,0.01, 0.1,1])
 
 subplot(1,2,2);
 hold on
 set(gca, 'YScale', 'log')
-plot(snapshots,Errors_q_m(2,:),'-o','LineWidth',2, 'markersize', 10);
+plot(snapshots,Errors_q_m(2,:),'-o','LineWidth',2, 'markersize', msize);
 set(gca, 'ColorOrderIndex', get(gca, 'ColorOrderIndex')+1);
-plot(snapshots,Errors_q_m(1,:),'--x','LineWidth',2, 'markersize', 10);
-plot(snapshots,Errors_q_m(3,:),':s','LineWidth',2, 'markersize', 10);
+plot(snapshots,Errors_q_m(1,:),'--x','LineWidth',2, 'markersize', msize);
+plot(snapshots,Errors_q_m(3,:),':s','LineWidth',2, 'markersize', msize);
+plot(snapshots,Errors_q_m(4,:),':^','LineWidth',2, 'markersize', msize);
 
 xlabel("Snapshots")
 ylabel("MSE amplitudes (Pa^2)")
-yticks([0.01, 0.1,1, 10,100])
+legend('SFW gr.','NOMP', 'OMP', 'MUSIC', 'Interpreter','tex')
 
